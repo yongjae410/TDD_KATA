c38caa848e851e73cf482bf930342c2680932d41
yongjae410 <yongjae410@gmail.com>
2023-07-05 15:08:26 +0900

    Test1 passed

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,10 @@
 #include "pch.h"
 #include "../TDD_KATA/PrimeFactor.cpp"
+#include <vector>
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+using namespace std;
+TEST(PrimeFactorTest, Test1) {
+	PrimeFactor pf;
+	vector<int> expected = { };
+	EXPECT_EQ(expected, pf.of(1));
 }
---
+++ TDD_KATA/PrimeFactor.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+
+using namespace std;
+
+class PrimeFactor
+{
+public:
+	vector<int> of(int i)
+	{
+		vector<int> result = { };
+		return result;
+	}
+};
